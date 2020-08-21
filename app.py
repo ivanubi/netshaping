c0b302de1d2d03d884e46fc698bcdfd60ef29bd5
Ivan Ubiñas <ivanubigen@gmail.com>
2020-08-21 12:16:52 -0400

    Improve tasks execution

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 import pathlib
+import os
 
 sys.path.append(str(pathlib.Path().absolute()))
 
@@ -30,10 +31,6 @@ celery.conf.beat_schedule = {
     "policy-stats": {
         "task": "app.policy_stats",
         "schedule": 30.0
-    },
-        "check-active-devices": {
-        "task": "app.check_active_devices",
-        "schedule": 30.0
     }
 }
 app.config.update(DEBUG=True, SECRET_KEY=b"ACEDEJEADEJE")
@@ -46,38 +43,51 @@ login_manager.init_app(app)
 
 from views import *
 from datetime import datetime
+from net import Connection
 
 @celery.task
 def policy_stats():
-    interfaces = Interface.query.filter(Interface.policy_id != None).all()
+    try:
+        f = open("task.txt", "r")
+        if f.read() == 'unlocked': 
+            print("— UNLOCKED CONFIRMED. RUNNING.")
+            with open("task.txt", "w") as file:
+                file.write("locked")
+                print("TASK LOCKED")
+            check_active_devices()
+            interfaces = Interface.query.filter(Interface.policy_id != None).all()
+            
+            for interface in interfaces:
+                device = interface.device
+                router = Connection(device.host, username = device.ssh_username, password = device.ssh_password)
+                snapshot = router.stats_policy_interface(interface.name)
+                if snapshot['success'] == True and ('classes' in snapshot):
+                    for class_name, stats in snapshot['classes'].items():
+                        interface.policy_stats.append(
+                            Stat(
+                                created_on=datetime.now(),
+                                policy_name=snapshot['output_policy'],
+                                class_name=stats['class_name'], 
+                                offered_rate = int(stats['offered_rate']), 
+                                drop_rate = int(stats['drop_rate'])
+                            )
+                        )
+                    interface.update()
+            with open("task.txt", "w") as file:
+                file.write("unlocked")
+                print("TASK UNLOCKED")
+        else:
+            print("— LOCKED CONFIRMED. STOPPING.")
+    except Exception as e:
+        print(e)
 
-    for interface in interfaces:
-        device = interface.device
-        router = Connection(device.host, username = device.ssh_username, password = device.ssh_password)
-        snapshot = router.stats_policy_interface(interface.name)
-        if snapshot['success'] == True and ('classes' in snapshot):
-            for class_name, stats in snapshot['classes'].items():
-                interface.policy_stats.append(
-                    Stat(
-                        created_on=datetime.now(),
-                        policy_name=snapshot['output_policy'],
-                        class_name=stats['class_name'], 
-                        offered_rate = int(stats['offered_rate']), 
-                        drop_rate = int(stats['drop_rate'])
-                    )
-                )
-            interface.update()
-
-@celery.task
 def check_active_devices():
-    try: 
-        devices = Device.query.all()
+    devices = Device.query.all()
+    for device in devices:
+        connection = Connection(device.host, username=device.ssh_username, password=device.ssh_password)
 
-        for device in devices:
-            if device.try_connection() == "success":
-                device.state = "active"
-            else:
-                device.state = "inactive"
-            device.update()
-    except:
-        pass
+        if connection.try_connection() == "success":
+            device.state = "active"
+        else:
+            device.state = "inactive"
+        device.update()
